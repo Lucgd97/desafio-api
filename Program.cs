01b6b5d72368334c6b98f295f728b3c4ac5f77c7
Lucas Gilio Ducci <lucasgducci@hotmail.com>
2023-05-06 09:59:01 -0300

    Revisando logica

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Console_desafio_api
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+            var numeroUm = 1;
+            int numeroDois = 2;
+
+            var soma = numeroUm + numeroDois;
+
+            System.Console.WriteLine($"A soma das variáveis é: {soma}");
+        }
+    }
+}
